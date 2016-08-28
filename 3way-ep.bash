243977a572218ff002ee4a16d95da3eb49d095a8
Eric Laquer <eric.laquer@marklogic.com>
2016-08-28 17:28:17 -0400

    Fixed user

--- 3way-ep.bash
+++ 3way-ep.bash
@@ -4,7 +4,7 @@ NAME=$1
 BASEPORT=$2
 
 # Read ML Host from pipeline
-read ML_HOST_JSON;
+read JSON_IN;
 
 # Collect Variables From Input JSON `
 HOSTURL_JSON=` echo $ML_HOST_JSON | jq '.host'| tr -d \" | jq -R -c '{host:.}' `
@@ -13,17 +13,17 @@ USERPW_JSON=` echo $ML_HOST_JSON | jq '.userpw'| tr -d \" | jq -R -c '{userpw:.}
 DOCUMENTS_PORT=$BASEPORT
 DOCUMENTS_EP_NAME="$NAME-DOCUMENTS"
 DOCUMENTS_EP_PROMPT="Create $NAME Document EP"
-CREATE_DOCUMENTS_EP_COMMAND_JSON=` echo $JSON | ./rest-ep-json.bash "$DOCUMENTS_EP_PROMPT" $DOCUMENTS_EP_NAME $DOCUMENTS_PORT | ./rest-ep-curl-command.bash `
+CREATE_DOCUMENTS_EP_COMMAND_JSON=` echo $JSON_IN | ./rest-ep-json.bash "$DOCUMENTS_EP_PROMPT" $DOCUMENTS_EP_NAME $DOCUMENTS_PORT | ./rest-ep-curl-command.bash `
 
 MODULES_EP_PORT=$(($BASEPORT+1))
 MODULES_EP_NAME="$NAME-MODULES"
 MODULES_EP_PROMPT="Create $NAME MODULES EP"
-CREATE_MODULES_EP_COMMAND_JSON=` echo $JSON | ./rest-ep-json.bash "$MODULES_EP_PROMPT" $MODULES_EP_NAME $MODULES_EP_PORT | ./rest-ep-curl-command.bash `
+CREATE_MODULES_EP_COMMAND_JSON=` echo $JSON_IN | ./rest-ep-json.bash "$MODULES_EP_PROMPT" $MODULES_EP_NAME $MODULES_EP_PORT | ./rest-ep-curl-command.bash `
 
 DEPLOY_EP_PORT=$(($BASEPORT+2))
 DEPLOY_EP_NAME="$NAME-DEPLOY"
 DEPLOY_EP_PROMPT="Create $NAME Deploy EP"
-CREATE_DEPLOY_EP_COMMAND_JSON=` echo $JSON | ./rest-ep-json.bash "$DEPLOY_EP_PROMPT" $DEPLOY_EP_NAME $DEPLOY_EP_PORT | ./rest-ep-curl-command.bash `
+CREATE_DEPLOY_EP_COMMAND_JSON=` echo $JSON_IN | ./rest-ep-json.bash "$DEPLOY_EP_PROMPT" $DEPLOY_EP_NAME $DEPLOY_EP_PORT | ./rest-ep-curl-command.bash `
 
 # Connect MODULES database to DOCUMENTS server 
 # Connect DEPLOY database to MODULES server 
