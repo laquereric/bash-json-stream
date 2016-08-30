f405ed3b85def7061144b220ba609419b28a5ca9
Eric Laquer <eric.laquer@marklogic.com>
2016-08-30 12:42:04 -0400

    connect modules to documents looks ok

--- 3way-ep.bash
+++ 3way-ep.bash
@@ -6,6 +6,9 @@ BASEPORT=$2
 # Read ML Host from pipeline
 read JSON_IN;
 
+SERVER_NAME="$NAME-Server"
+SERVER_NAME_JSON=` echo $SERVER_NAME | jq -R -c '{"server-name":.}' `
+
 DOCUMENTS_PORT=$BASEPORT
 DOCUMENTS_EP_NAME="$NAME-Document"
 DOCUMENTS_EP_PROMPT="echo Create $NAME Document EP"
@@ -25,11 +28,17 @@ DEPLOY_EP_PROMPT_64=` echo $DEPLOY_EP_PROMPT | base64 --wrap=0 `
 CREATE_DEPLOY_EP_COMMAND_JSON=` echo $JSON_IN | ./rest-ep-json.bash $DEPLOY_EP_PROMPT_64 $DEPLOY_EP_NAME $DEPLOY_EP_PORT | ./manage-v2-servers-create-curl-command.bash `
 
 # Connect MODULES database to DOCUMENTS server
-#manage-v2-servers-change-properties-curl-command.bash 
-# Connect DEPLOY database to MODULES server 
-#manage-v2-servers-change-properties-curl-command.bash
+MODULES_DATABASE_NAME="$MODULES_EP_NAME-Database"
+CONNECT_MODULES_2_DOCUMENTS_PROMPT="echo Create $NAME Document EP"
+CONNECT_MODULES_2_DOCUMENTS_PROMPT_64=` echo $CONNECT_MODULES_2_DOCUMENTS_PROMPT | base64 --wrap=0 `
 
-CREATE_EPS_LIST=`echo $CREATE_DOCUMENTS_EP_COMMAND_JSON $CREATE_MODULES_EP_COMMAND_JSON $CREATE_DEPLOY_EP_COMMAND_JSON `
+CONNECT_MODULES_2_DOCUMENTS_JSON=`echo $SERVER_NAME_JSON | jq -r -c --arg MDN $MODULES_DATABASE_NAME '.+{"modules-database":$MDN}' | jq -r -c --arg MDP $CONNECT_MODULES_2_DOCUMENTS_PROMPT_64 '.+{properties:{prompt:$MDP}}' `
+#echo $CONNECT_MODULES_2_DOCUMENTS_JSON
+
+CONNECT_MODULES_2_DOCUMENTS_COMMAND_JSON=` echo $CONNECT_MODULES_2_DOCUMENTS_JSON | ./manage-v2-servers-change-properties-curl-command.bash `
+
+CREATE_EPS_LIST=`echo $CREATE_DOCUMENTS_EP_COMMAND_JSON $CREATE_MODULES_EP_COMMAND_JSON $CREATE_DEPLOY_EP_COMMAND_JSON $CONNECT_MODULES_2_DOCUMENTS_COMMAND_JSON`
 CREATE_EPS=` echo $CREATE_EPS_LIST | jq -r --slurp '.' `
+
 
 echo $CREATE_EPS
--- manage-v2-servers-change-properties-curl-command.bash
+++ manage-v2-servers-change-properties-curl-command.bash
@@ -1,1 +1,40 @@
 #!/bin/bash
+#https://docs.marklogic.com/REST/POST/manage/v2/servers
+
+read INPUT_JSON;
+
+# Collect Variables From Input JSON
+PROPERTIES_JSON=` echo $INPUT_JSON | jq -c '.properties' | jq -c '{properties:.}'`
+
+HOSTURL=` echo $INPUT_JSON | jq -r -c '.host'| tr -d \" `
+USERPW=` echo $INPUT_JSON | jq -r -c '.userpw'| tr -d \" `
+PORT=` echo $INPUT_JSON | jq -r -c '.port' | tr -d \" `
+SERVER_NAME=` echo $INPUT_JSON | jq -r -c '.["server-name"]' | tr -d \" `
+DATA_JSON=`echo $INPUT_JSON | jq -r -c '. | del(.properties) | del(.["server-name"]) | del(.host) | del(.userpw)' `
+# | .+{root:'\'
+ 
+HEADER="Content-Type:application/json"
+
+COMMAND=$(cat <<EOF
+	curl -v -X POST \
+	-u $USERPW \
+	--header "$HEADER" \
+	-d '${DATA_JSON}' \	
+	'http://localhost:8002/manage/v2/servers/$SERVER_NAME/properties
+EOF
+)
+
+# Parameters not passed
+# ?group-id=Default'
+
+COMMAND_64=` echo $COMMAND | base64 --wrap=0 `
+COMMAND_JSON=` echo "$COMMAND_64" | jq -c -R '{command:.}' `
+
+# Assemble JSON Output
+COMPONENT_LIST=` echo $PROPERTIES_JSON $COMMAND_JSON `
+JSON_COMPONENTS=` echo $COMPONENT_LIST | jq --slurp '.' `
+
+JSON=` echo $JSON_COMPONENTS | jq -c '.[0] + .[1]' `
+
+#Return Output
+echo $JSON
