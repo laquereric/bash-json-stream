a2277023a03ab846edf874857269b4732c4e9ae3
Eric Laquer <eric.laquer@marklogic.com>
2016-09-01 00:30:03 -0400

    start document work

---
+++ load-documents.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HOST="localhost"
+USERPW="admin:admin"
+NAME="TEST2"
+PORT=8011
+DIR=`ls ./Documents`
+echo $DIR
+echo "Done Loading documents into Service $NAME on Port $PORT of HostUrl $HOST"
